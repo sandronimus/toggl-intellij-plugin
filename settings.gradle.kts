4d62c17167d1c0f4d1c30348d645262f72aa77d1
GitHub Action <action@github.com>
2022-07-12 19:51:21 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "toggl-intellij-plugin"
---
+++ src/main/kotlin/com/github/sandronimus/togglintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sandronimus.togglintellijplugin.services
+
+import com.github.sandronimus.togglintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
